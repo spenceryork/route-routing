32a4a35fb25559d3ed1718b915b1694f3b35739b
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2018-01-29 18:35:31 -0600

    Routes and Route-Listing both working

--- app/app.js
+++ app/app.js
@@ -3,12 +3,16 @@
 angular.module("RouteApp", ["ngRoute"])
 .config( ($routeProvider) => {
     $routeProvider
-   .when('/blueridgeparkway', {
+    .when('/', {
+        templateUrl: "app/partials/home.html"
+    })
+    .when('/blueridgeparkway', {
        templateUrl: "app/partials/blueRidge.html",
        controller: "BlueRidgeCtrl"
-   })
-   .when('/route66', {
+    })
+    .when('/route66', {
        templateUrl: "app/partials/route66.html",
        controller: "Route66Ctrl"
-   });
+    })
+    .otherwise('/');
 });
